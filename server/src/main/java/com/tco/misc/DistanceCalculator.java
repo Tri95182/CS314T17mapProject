477601e1e836b0a687f51165c630fc713967b805
Jack Melvin <melvin42@cs.colostate.edu>
2020-09-27 16:51:29 -0600

    Added Distance Request and test based off lecture

---
+++ server/src/main/java/com/tco/misc/DistanceCalculator.java
@@ -0,0 +1,30 @@
+package com.tco.misc;
+
+import java.util.Map;
+import java.lang.Math;
+
+public class DistanceCalculator {
+  public int calculate(Float radius, Map<String, String> place1, Map<String, String> place2) {
+    double lat1 = Double.parseDouble(place1.get("Latitude"));
+    double lon1 = Double.parseDouble(place1.get("Longitude"));
+    double lat2 = Double.parseDouble(place2.get("Latitude"));
+    double lon2 = Double.parseDouble(place2.get("Longitude"));
+
+    double sinLat1 = Math.sin(lat1);
+    double cosLat1 = Math.cos(lat1);
+    double sinLat2 = Math.sin(lat2);
+    double cosLat2 = Math.cos(lon2);
+    double deltaLon = lon2 - lon1;
+    double sinDeltaLon = Math.sin(deltaLon);
+    double cosDeltaLon = Math.cos(deltaLon);
+
+    double centralAngleNumerator = Math.sqrt(Math.pow(cosLat2 * sinDeltaLon,2)
+            + Math.pow(cosLat1 * sinLat2 - sinLat1 * cosLat2 * cosDeltaLon,2));
+    double centralAngleDenominator = sinLat1 * sinLat2 + cosLat1 * cosLat2 * cosDeltaLon;
+
+    double centralAngle = Math.atan2(centralAngleNumerator,centralAngleDenominator);
+    double distance = radius * centralAngle;
+
+    return (int)distance;
+  }
+}
