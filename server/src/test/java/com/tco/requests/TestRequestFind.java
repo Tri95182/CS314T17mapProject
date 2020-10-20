998238d1f323e3cb838d6f8298aedc34f18b7a46
Jack Melvin <jackrmelvin@gmail.com>
2020-10-19 18:32:14 -0600

    Updated test request versions

--- server/src/test/java/com/tco/requests/TestRequestFind.java
+++ server/src/test/java/com/tco/requests/TestRequestFind.java
@@ -46,10 +46,10 @@ public class TestRequestFind {
     }
 
     @Test
-    @DisplayName("Version number is equal to 2")
+    @DisplayName("Version number is equal to 3")
     public void testVersion() {
       int version = find.getRequestVersion();
-      assertEquals(2, version);
+      assertEquals(3, version);
     }
 
     @Test
