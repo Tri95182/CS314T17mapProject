b04fd6b6dff3b2f7a48001911f061839ca782ed6
collinw99 <collinw99@gmail.com>
2020-10-21 21:55:37 -0600

    Add tests for polyline

--- client/test/Atlas.test.js
+++ client/test/Atlas.test.js
@@ -4,6 +4,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import Atlas from '../src/components/Atlas/Atlas';
+import { Polyline } from 'react-leaflet';
 
 const startProperties = {
   createSnackBar: jest.fn()
@@ -31,7 +32,7 @@ function testMarkerIsRenderedOnClick() {
 
   expect(actualMarkerPosition).toEqual(expectedMarkerPosition);
 
-  let latlng = {lat: 0, lng: 0};
+  let latlng = {name:"Marker Location", lat: 0, lng: 0};
   simulateOnClickEvent(atlas, {latlng: latlng});
 
   expect(atlas.state().markerPosition).toEqual(latlng);
@@ -66,5 +67,35 @@ test("Test geolocation sets user state", testGeolocation);
 
 function testPolyline(){
   const atlas = shallow(<Atlas createSnackBar={startProperties.createSnackBar}/>);
+  const testPlaces = [
+    {name:"one", lat:10, lng:20},
+    {name:"two", lat:20, lng:30},
+    {name:"three", lat:30, lng:40},
+  ];
+  atlas.setState({placesDistance: testPlaces});
 
+  expect(atlas.find(Polyline).length).toEqual(3);
 }
+
+test("Test render of polylines", testPolyline);
+
+
+function testChangeInArray() {
+  
+  const atlas = shallow(<Atlas createSnackBar={startProperties.createSnackBar}/>);
+  const testPlaces = [
+    {name:"one", lat:10, lng:20},
+    {name:"two", lat:20, lng:30},
+    {name:"three", lat:30, lng:40},
+  ];
+  atlas.setState({placesDistance: testPlaces});
+
+  expect(atlas.state().placesDistance).toEqual(testPlaces);
+
+  const newItem = {name:"twoTest", lat:200, lng:300};
+  atlas.instance().changeInArray(atlas.state().placesDistance, testPlaces[1], newItem, "placesDistance");
+
+  expect(atlas.state().placesDistance[1]).toEqual(newItem);  
+}
+
+test("Test change in array function", testChangeInArray);
