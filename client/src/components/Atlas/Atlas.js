5e642a456cb0bc828efa38536713088dcde2850f
collinw99 <collinw99@gmail.com>
2020-12-10 18:00:57 -0700

    Change earth radius to be insettings state

--- client/src/components/Atlas/Atlas.js
+++ client/src/components/Atlas/Atlas.js
@@ -56,9 +56,9 @@ export default class Atlas extends Component {
       infoModalOpen: false,
       info: null,
       settingsModalOpen: false,
-      earthRadius: 6371.0,
       tripTitle: "Trip",
       settings: {
+        earthRadius: 6371.0,
         units: 'Kilometers',
         optTrip: false,
         showMarkers: true,
@@ -166,7 +166,7 @@ export default class Atlas extends Component {
             createSnackBar={this.props.createSnackBar}
             mapRef={this.mapRef}
             sendRequest={this.sendRequest}
-            earthRadius={this.state.earthRadius}
+            earthRadius={this.state.settings.earthRadius}
             tripTitle={this.state.tripTitle}
         />
     );
@@ -183,8 +183,9 @@ export default class Atlas extends Component {
             optTrip={this.state.settings.optTrip}
             units={this.state.settings.units}
             calcTrip ={this.state.settings.calcTrip}
-            earthRadius={this.state.earthRadius}
+            earthRadius={this.state.settings.earthRadius}
             tripTitle={this.state.tripTitle}
+            settings={this.state.settings}
         />
     );
   }
